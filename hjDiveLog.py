58637f3ade46ff93b3a10a3de17411aa05149e7f
henryjuho <juhob@hotmail.co.uk>
2017-06-21 23:16:05 +0100

    added dive record format and parameter to pass to dive window

--- hjDiveLog.py
+++ hjDiveLog.py
@@ -4,10 +4,17 @@ import sys
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 import pyqtgraph as pg
+import numpy as np
 
 
 class DiveWindow(QtGui.QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, dive_record, parent=None):
+
+        """
+        creates a QTabWidget
+        :param dive_record: dict
+        :param parent:
+        """
         super(DiveWindow, self).__init__(parent)
 
         dw_layout = QtGui.QVBoxLayout()
@@ -16,38 +23,33 @@ class DiveWindow(QtGui.QWidget):
         pg.setConfigOption('background', 'w')
         pg.setConfigOption('foreground', 'k')
 
-        profile_plot = pg.PlotWidget()
-        profile_plot.setLabel('left', 'Depth', 'm')
-        profile_plot.setLabel('bottom', 'Time', 'mins')
-        #  profile_plot.setFont(self.main_font)
-
         # top row of stat boxes
         top_boxes = QtGui.QHBoxLayout()
-        top_boxes.addWidget(self.dive_stats_box())
-        top_boxes.addWidget(self.gas_stats_box())
+        top_boxes.addWidget(self.dive_stats_box(dive_record))
+        top_boxes.addWidget(self.gas_stats_box(dive_record))
 
         # notes box
-        dive_notes = self.notes_box()
+        dive_notes = self.notes_box(dive_record['notes'])
 
         # add everything to main layout
         dw_layout.addLayout(top_boxes)
-        dw_layout.addWidget(profile_plot)
+        dw_layout.addWidget(self.profile_box(dive_record['profile']))
         dw_layout.addWidget(dive_notes)
         self.setLayout(dw_layout)
 
-    def dive_stats_box(self):
+    def dive_stats_box(self, record):
 
-        labels = ['time in', 'dive time:', 'time out:', 'max depth:', 'average depth:', 'temp:']
-        values = ['12:30', '01:30', '14:00', '18', '12', '13C']
+        labels = ['time_in', 'dive_time', 'time_out', 'max_depth', 'average_depth', 'temp']
+        values = [record[x] for x in labels]
 
         dive_box = self.stats_box(labels, values, 'Dive stats', 3)
 
         return dive_box
 
-    def gas_stats_box(self):
+    def gas_stats_box(self, record):
 
-        labels = ['start pressure:', 'end pressure:', 'volume:', 'SAC rate:']
-        values = ['210', '40', '24', '21.5']
+        labels = ['start_pressure', 'end_pressure', 'volume', 'SAC_rate']
+        values = [record[x] for x in labels]
 
         gas_box = self.stats_box(labels, values, 'Gas stats', 2)
 
@@ -61,7 +63,7 @@ class DiveWindow(QtGui.QWidget):
         rows_per_box = no_rows
 
         for i in range(len(labels)):
-            lab, val = labels[i], values[i]
+            lab, val = labels[i].replace('_', ' ') + ':', values[i]
 
             qt_lab = QtGui.QLabel(lab, self)
             qt_val = QtGui.QLabel(val, self)
@@ -80,9 +82,22 @@ class DiveWindow(QtGui.QWidget):
 
         return stats_box
 
-    def notes_box(self):
+    def profile_box(self, profile):
 
-        note_text = 'This was a very exciting dive with over 1m vis'
+        box = QtGui.QGroupBox('Dive profile')
+        prof_layout = QtGui.QVBoxLayout()
+
+        profile_plot = pg.PlotWidget()
+        profile_plot.setLabel('left', 'Depth', 'm')
+        profile_plot.setLabel('bottom', 'Time', 'mins')
+        profile_plot.plot(profile[0], profile[1])
+        prof_layout.addWidget(profile_plot)
+
+        box.setLayout(prof_layout)
+
+        return box
+
+    def notes_box(self, note_text):
 
         note_layout = QtGui.QVBoxLayout()
         notes = QtGui.QTextEdit()
@@ -126,6 +141,13 @@ class Window(QtGui.QMainWindow):
         self.setGeometry(100, 100, 1050, 700)  # coords start from top left x, y, width, height
         self.setWindowTitle('hjDiveLog')
 
+        # test data
+        test_dive_record = {'time_in': '12:00', 'dive_time': '01:22', 'time_out': '13:22',
+                            'max_depth': '12', 'average_depth': '8', 'temp': '14C',
+                            'start_pressure': '200', 'end_pressure': '60', 'volume': '24',
+                            'SAC_rate': '21', 'notes': 'This was a very exciting dive with over 3m vis',
+                            'profile': (np.cumsum((0, 2, 72, 3, 3, 3)), (0, -12, -12, -6, -6, 0))}
+
         # set toolbar
         toolbar = QtGui.QToolBar(self)
         exit_planner = QtGui.QAction(QtGui.QIcon('images/exit.png'), 'exit', self)
@@ -152,7 +174,7 @@ class Window(QtGui.QMainWindow):
         log_windows = QtGui.QVBoxLayout()
 
         self.dive_tabs = QtGui.QTabWidget()
-        self.dive_tabs.tab1 = DiveWindow()
+        self.dive_tabs.tab1 = DiveWindow(test_dive_record)
         self.dive_tabs.tab2 = QtGui.QWidget()
         self.dive_tabs.tab3 = QtGui.QWidget()
 
